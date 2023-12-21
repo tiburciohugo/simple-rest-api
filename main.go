ac6792e53ca1d994f30d3d03e9764b7fce82690d
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-12-21 12:34:09 -0300

    Add HTTP server and request handler

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/tiburciohugo/simple-rest-api/router"
 )
 
-func main() {
+func HandleRequest(w http.ResponseWriter, r *http.Request) {
 	router.Initialize()
+}
+
+func main() {
+	http.HandleFunc("/", HandleRequest)
+	http.ListenAndServe(":8080", nil)
 }
